09b4de5d5001aac9f8087059261dcf55b6fd3a7d
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-10-01 19:12:47 +0200

    Added auth and follow

--- index.js
+++ index.js
@@ -12,8 +12,46 @@ const redirectHTML = url => `<!doctype html>
 </html>
 `
 
+const connectHTML = `<!doctype html>
+<html>
+  <head>
+    <script src="https://connect.soundcloud.com/sdk/sdk-3.1.2.js"></script>
+    <script>
+      SC.initialize({
+        client_id: 'bb00ef3aa833887211ec2eebc049ae7a',
+        redirect_uri: 'http://localhost:8000/docs/gate-auth/callback.html'
+      });
+
+      SC.connect().then(function(){
+        return SC.put('/me/followings/21200711');
+      }).then(function(user){
+        alert('You are now following ' + user.username);
+      }).catch(function(error){
+        alert('Error: ' + error.message);
+      });
+    </script>
+  </head>
+  <body>
+  </body>
+</html>
+`
+
+const callbackHTML = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Connect with SoundCloud</title>
+  </head>
+  <body onload="window.setTimeout(window.opener.SC.connectCallback, 1)">
+    <b style="text-align: center;">This popup should automatically close in a few seconds</b>
+  </body>
+</html>
+`
+
 fs.emptyDirSync('docs')
 fs.outputFileSync('docs/CNAME', 'leafadventure.com\n')
+fs.outputFileSync('docs/gate-auth/index.html', connectHTML)
+fs.outputFileSync('docs/gate-auth/callback.html', callbackHTML)
 fs.outputFileSync('docs/index.html', redirectHTML('https://soundcloud.com/leafadventure'))
 
 redirs.forEach((redir) => {
