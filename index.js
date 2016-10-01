4e9402dd3a3440941ce3ce9cfef4fbcdd51f1cdc
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-10-01 16:10:56 +0200

    Fully functional redirects

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ const fs = require('fs-extra')
 
 const redirs = require('./redirs')
 
+const redirectHTML = url => `<!doctype html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; URL='${url}'" />
+  </head>
+  <body>
+  </body>
+</html>
+`
+
+fs.emptyDirSync('docs')
+fs.outputFileSync('docs/CNAME', 'leafadventure.com\n')
+fs.outputFileSync('docs/index.html', redirectHTML('https://soundcloud.com/leafadventure'))
+
 redirs.forEach((redir) => {
   redir.urls.forEach((url) => {
-    fs.outputFileSync(`docs/${url}/index.html`, `
-      <!doctype html>
-      <html>
-        <head>
-          <meta http-equiv="refresh" content="0; URL='${redir.dest}'" />
-        </head>
-        <body>
-        </body>
-      </html>
-    `)
+    fs.outputFileSync(`docs/${url}/index.html`, redirectHTML(redir.dest))
   })
 })
 
 // TODO Create gate-auth/index.html
 // TODO Create gate-follow/index.html
-// TODO Make the root index.html redirect to 'https://soundcloud.com/leafadventure'
